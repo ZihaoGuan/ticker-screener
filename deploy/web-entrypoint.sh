fba175cf996c4b41023e97ae6179e126c30db999
Zihao.Guan <zihao.guan@everbridge.com>
2026-07-11 00:02:35 +1200

    improve

--- deploy/web-entrypoint.sh
+++ deploy/web-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-if ! command -v node >/dev/null 2>&1 || ! command -v chromium >/dev/null 2>&1; then
+if ! command -v node >/dev/null 2>&1 || ! command -v chromium >/dev/null 2>&1 || ! command -v curl >/dev/null 2>&1; then
   apt-get update
   apt-get install -y --no-install-recommends nodejs npm chromium curl
   rm -rf /var/lib/apt/lists/*
